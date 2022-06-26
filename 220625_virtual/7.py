78de896e5e26b5db0760bca7f6108e63fe1da1c4
Shinichiro Saito <s-saito@chic.ocn.ne.jp>
2022-06-26 21:27:21 +0900

    add

---
+++ 220625_virtual/7.py
@@ -0,0 +1,11 @@
+from atcoder.atcoder_test import def_input, input
+
+input_text = """
+1
+1 1000000
+"""
+
+def_input(input_text)
+
+if __name__ == "__main__":
+    
--- snippet.py
+++ snippet.py
@@ -12,6 +12,12 @@ num_list = list(map(int, input().split()))
 # 数字の配列のlist
 num_arr_list = [list(map(int, input().split())) for _ in range(n)]
 
+# 盤面のlist
+board_list = [list(input()) for _ in range(r)]
+
+# 1始まりを0始まりに変換
+sy, sx = map(lambda x: int(x) - 1, input().split())
+
 # 方向の探索
 
 # 4方向
@@ -21,3 +27,7 @@ dx = (1, 0, -1, 0)
 # 8方向
 dy = (0, 1, 1, 1, 0, -1, -1, -1)
 dx = (1, 1, 0, -1, -1, -1, 0, 1)
+
+# 盤面からはみ出さないようにする
+if ny < 0 or ny >= h or nx < 0 or nx >= w:
+    continue
