214108cbca7d6cacf1c9cfde8fbbddb3e755358b
Shinichiro Saito <s-saito@chic.ocn.ne.jp>
2022-07-24 16:24:42 +0900

    add

--- snippet.py
+++ snippet.py
@@ -50,8 +50,9 @@ n // w  # 切り捨て
 # 108.0
 
 # 最小値を求める場合の答えの初期値
-ans = float("inf")
 
+INF = float("inf")
+MINUS_INF = -float("inf")
 
 # 方向の探索
 
@@ -99,6 +100,7 @@ def grid_bfs(grid: list, sy: int, sx: int, INF: int = 1 << 60, wall: str = "#") 
     q.append((sy, sx))
 
     INF = 1 << 60  # 2**60
+
     dist = [[INF] * w for _ in range(h)]
     dist[sy][sx] = 0
 
@@ -213,3 +215,61 @@ if __name__ == "__main__":
             print("Yes")
             exit()
     print("No")
+
+
+# 動的計画法
+
+# 問題
+# https://atcoder.jp/contests/abc261/tasks/abc261_d
+
+from collections import defaultdict
+
+if __name__ == "__main__":
+
+    # 答えの動画
+    # 動画: https://www.youtube.com/watch?v=P20UCEOnUzU
+
+    # 個人的なメモ
+    # DPには、配る、もらう、メモ化再帰の３種類がある
+
+    n, m = map(int, input().split())
+    # [0]を最初に追加することで、0-indexにする
+    x = [0] + list(map(int, input().split()))
+
+    bonus = defaultdict(int)
+    for _ in range(m):
+        c, y = map(int, input().split())
+        bonus[c] = y
+
+    MINUS_INF = -float("inf")
+
+    dp = [MINUS_INF] * 5500  # dp[j] := i回目にj連続H(head,表)だった時に持っているコインの最大値
+
+    # 1回目にH
+    # bonusは呼ばれても、該当しない場合は0を返す
+    dp[1] = x[1] + bonus[1]
+
+    # 1回目にT
+    dp[0] = 0
+
+    # dpは一つ前の列、nxtは今回調査する列を調べる
+
+    for i in range(1, n):  # 配るDP: i回目からi+1回目への遷移を考える
+        nxt = [MINUS_INF] * 5500
+
+        for j in range(n):
+            # i, j := i回目にj連続Hが出ている
+            if dp[j] == MINUS_INF:
+                continue
+
+            # i+1回目はHの場合
+            # maxでnxt[j+1]を行うのは、既にnxt[j+1]に値がある場合を想定している
+            # 動画より、maxの候補としては、前回の数字(dp[j])をベースに、今回のxの値(x[i+1])とbonusを足した数字になる
+            nxt[j + 1] = max(nxt[j + 1], dp[j] + x[i + 1] + bonus[j + 1])
+
+            # i+1回目はT(tail,裏)
+            nxt[0] = max(nxt[0], dp[j])
+
+        dp = nxt
+
+    print(max(dp))
